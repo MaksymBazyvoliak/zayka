93caed374a1f3e2013e5ca3820a4d3fd7cffd84f
BenEngry <bazivolyakm@gmail.com>
2022-06-25 00:07:39 +0300

    added card-slider

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+const sliderImages = document.querySelectorAll(".slider__img"),
+    sliderLine = document.querySelector(".slider__line");
+
+let width = document.querySelector(".slider").offsetWidth;
+
+let count = 0;
+
+document.querySelector(".slider-next").addEventListener("click", () => {
+    count+=3;
+    if (count >= sliderImages.length ) {
+        count = 0;
+    }
+    rollSlide(count);
+});
+
+document.querySelector(".slider-prev").addEventListener("click", () => {
+    count-=3;
+    if (count < 0 ) {
+        count = sliderImages.length-3;
+    }
+    rollSlide(count);
+});
+
+function rollSlide(count) {
+    sliderLine.style.transform = "translate(-" + count/3 * width + "px)";
+}
+
